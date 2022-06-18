14d3e7d3cd7c6937883c5a3ea06810816f4b329a
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2022-06-18 15:00:59 -0300

    Proyect in progress

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// Assets images path for the application
+// https://raw.githubusercontent.com/PokeAPI/sprites/068d04e360891931c8be1533a944fa28b9bae0b2/sprites/pokemon/other/dream-world/649.svg
+// Api url for the application
+// https://pokeapi.co/api/v2/pokemon/649
